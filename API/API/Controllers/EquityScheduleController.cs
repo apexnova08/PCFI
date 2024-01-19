56bbbd253b6fb7d34fecb3118947751ea60d9b75
Tony Diesta <57242931+apexnova08@users.noreply.github.com>
2024-01-19 11:35:13 +0800

    Add files via upload

---
+++ API/API/Controllers/EquityScheduleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API.Models;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EquityScheduleController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Equity> Get(double SellingPrice, DateTime ReservationDate, int EquityTerm)
+        {
+            double monthlyAmount = SellingPrice / EquityTerm;
+            return Enumerable.Range(1, EquityTerm).Select(index => new Equity
+            {
+                Balance = SellingPrice - monthlyAmount * index,
+                DueDate = ReservationDate.AddMonths(index),
+                Term = index,
+                MonthlyAmount = monthlyAmount
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ API/API/Models/Equity.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models
+{
+    public class Equity
+    {
+        public double Balance { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Term { get; set; }
+        public double MonthlyAmount { get; set; }
+        public double Interest => (int)(MonthlyAmount * 0.05);
+        public double Insurance => (int)(MonthlyAmount * 0.01);
+    }
+}
