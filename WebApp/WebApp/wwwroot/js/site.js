d63a08a161becdcaff9098d7344fa76d0b81d997
Tony Diesta <57242931+apexnova08@users.noreply.github.com>
2024-01-19 11:49:20 +0800

    Add files via upload

---
+++ WebApp/WebApp/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+class Sched {
+    constructor(balance, duedate, term, amount, interest, insurance) {
+        this.balance = balance;
+        this.duedate = duedate;
+        this.term = term;
+        this.amount = amount;
+        this.interest = interest;
+        this.insurance = insurance;
+    }
+}
+function OnLoad() {
+
+    // Define the API URL
+    const apiUrl = 'https://localhost:7084/EquitySchedule?SellingPrice=20000&ReservationDate=2022-2-2&EquityTerm=5';
+
+    // Make a GET request
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                document.getElementById("test").textContent = "not epic";
+            }
+            document.getElementById("test").textContent = "epic";
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
