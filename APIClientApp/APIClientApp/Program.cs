198b0f018327bd0f94ffa0e71b0fdcfc537e7314
Tony Diesta <57242931+apexnova08@users.noreply.github.com>
2024-01-19 11:43:26 +0800

    Add files via upload

---
+++ APIClientApp/APIClientApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using APIClientApp;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:7084");
+
+client.DefaultRequestHeaders.Clear();
+
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+HttpResponseMessage response = client.GetAsync("EquitySchedule?SellingPrice=10000&ReservationDate=2022-2-2&EquityTerm=5").Result;
+
+if (response.IsSuccessStatusCode)
+{
+    var scheds = await response.Content.ReadFromJsonAsync<IEnumerable<GetResponse>>();
+    foreach (var sched in scheds)
+    {
+        Console.WriteLine(sched.Balance);
+        Console.WriteLine(sched.DueDate.ToShortDateString());
+        Console.WriteLine(sched.Term);
+        Console.WriteLine(sched.MonthlyAmount);
+        Console.WriteLine(sched.Interest);
+        Console.WriteLine(sched.Insurance);
+        Console.WriteLine();
+    }
+    Console.WriteLine(response);
+}
+else
+{
+    Console.WriteLine("Not Epic");
+}
